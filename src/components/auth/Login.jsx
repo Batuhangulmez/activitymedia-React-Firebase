7b2d2f2031186e18d989284725353034882f1787
Batuhangulmez <batuhanngulmez@gmail.com>
2021-06-03 00:10:55 +0300

    form operations

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
 import casemicelogo from "../../images/casemice.png"
 import { LoginContainer } from './LoginContainer'
 import {
@@ -11,6 +13,18 @@ import {
 
 
 export const Login = () => {
+
+
+    const { register, formState: { errors }, handleSubmit, setValue } = useForm();
+
+
+
+    const onSubmit = (data, e) => {
+        console.log(data);
+        console.log(errors);
+    };
+
+
     return (
         <LoginContainer>
             <div className="flex flex-col  bg-white rounded-md p-11 shadow-2xl ">
@@ -18,18 +32,56 @@ export const Login = () => {
                     <img src={casemicelogo} alt="Casemice Logo" className="absolute w-24 h-24 rounded-md bg-white p-2  -mt-11" />
                 </div>
 
-                <form className="flex flex-col">
-                    <h2 className="text-3xl mb-5">Giriş Yap</h2>
+
+
+                <form className="flex flex-col"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
+                    <h2 className="mx-auto text-3xl mb-5">Giriş Yap</h2>
                     <label className="flex my-2 group">
                         <div className=" mr-2 group-hover:text-primary-base  "><EmailIcon /></div>
-                        <input type="email" placeholder="Email" name="email" className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600  focus:border-primary-base " />
+                        <input
+                            type="email"
+                            placeholder="Email Adresi"
+                            name="email"
+                            className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600  focus:border-primary-base "
+                            onChange={(event, { name, value }) => {
+                                setValue(name, value);
+                            }}
+                            {...register("email", {
+                                required: true,
+                            })}
+                        />
                     </label>
+
                     <label className="flex my-2 group">
                         <div className="mr-2 group-hover:text-primary-base"><PasswordIcon /></div>
-                        <input type="password" placeholder="Password" name="password" className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600 focus:border-primary-base" />
+                        <input
+                            type="password"
+                            placeholder="Şifre"
+                            name="password"
+                            className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600 focus:border-primary-base"
+                            onChange={(event, { name, value }) => {
+                                setValue(name, value);
+                            }}
+                            {...register("password", {
+                                required: true,
+                                minLength: 3,
+                            })}
+                        />
                     </label>
+                    <div className="flex flex-col items-center text-xs text-primary-base border-l-2 border-r-2 border-gray-400 rounded-xl ">
+                        <p>
+                            {errors.email && "Email adresi hatalı"}
+                        </p>
+                        <p>
+                            {errors.password && "Şifrenizi yanlış girdiniz"}
+                        </p>
+
+                    </div>
+
                     <input className="mt-4 p-2  bg-primary-base hover:bg-primary-dark text-white hover:text-white border rounded-tl-3xl rounded-md rounded-br-3xl" type="submit" value="Submit" />
-                    <div className="mx-auto mt-4 ">Yeni misin?    <a href="/signup" className="text-primary-base">Hesap Oluştur</a></div>
+                    <div className="mx-auto mt-4 ">Yeni misin?    <Link to="/signup" className="text-primary-base">Hesap Oluştur</Link></div>
                 </form>
 
             </div>
--- src/components/auth/SignUp.jsx
+++ src/components/auth/SignUp.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { useForm } from 'react-hook-form';
 import casemicelogo from "../../images/casemice.png"
 
 import { LoginContainer } from './LoginContainer'
@@ -11,28 +13,73 @@ import {
 
 
 export const SignUp = () => {
+
+    const { register, errors, handleSubmit, setValue } = useForm();
+
+
+    const onSubmit = (data) => {
+        console.log(data)
+    }
+
     return (
         <LoginContainer>
             <div className="flex flex-col  bg-white rounded-md p-11 shadow-2xl ">
                 <div className="flex  bg-black mx-auto justify-center items-center">
                     <img src={casemicelogo} alt="Casemice Logo" className="absolute w-24 h-24 rounded-md bg-white p-2  -mt-11" />
                 </div>
-                <form className="flex flex-col">
+                <form className="flex flex-col"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <h2 className="text-3xl mb-5">Kayıt Ol</h2>
                     <label className="flex my-2 group">
                         <div className=" mr-2 group-hover:text-primary-base  "><ProfilIcon /></div>
-                        <input type="text" placeholder="İsim" name="name" className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600  focus:border-primary-base" />
+                        <input
+                            type="text"
+                            placeholder="Kullanıcı adı"
+                            name="username"
+                            className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600  focus:border-primary-base"
+                            onChange={(event, { name, value }) => {
+                                setValue(name, value);
+                            }}
+                            {...register("username", {
+                                required: true,
+                            })}
+                        />
+
                     </label>
                     <label className="flex my-2 group">
                         <div className=" mr-2 group-hover:text-primary-base  "><EmailIcon /></div>
-                        <input type="email" placeholder="Email" name="email" className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600  focus:border-primary-base" />
+                        <input
+                            type="email"
+                            placeholder="Email"
+                            name="email"
+                            className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600  focus:border-primary-base"
+                            onChange={(event, { name, value }) => {
+                                setValue(name, value);
+                            }}
+                            {...register("email", {
+                                required: true,
+                            })}
+                        />
                     </label>
                     <label className="flex my-2 group">
                         <div className="mr-2 group-hover:text-primary-base"><PasswordIcon /></div>
-                        <input type="password" placeholder="Password" name="password" className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600 focus:border-primary-base " />
+                        <input
+                            type="password"
+                            placeholder="Password"
+                            name="password"
+                            className="  bg-transparent w-full p-1 focus:outline-none border-b-2 border-gray-600 focus:border-primary-base "
+                            onChange={(event, { name, value }) => {
+                                setValue(name, value);
+                            }}
+                            {...register("password", {
+                                required: true,
+                                minLength: 3,
+                            })}
+                        />
                     </label>
                     <input className="mt-4 p-2  bg-primary-base hover:bg-primary-dark text-white hover:text-white border rounded-tl-3xl rounded rounded-br-3xl" type="submit" value="Submit" />
-                    <div className="mx-auto mt-4 ">Hesabın var mı?    <a href="/login" className="text-primary-base">Giriş Yap</a></div>
+                    <div className="mx-auto mt-4 ">Hesabın var mı?    <Link to="/login" className="text-primary-base">Giriş Yap</Link></div>
                 </form>
 
             </div>
